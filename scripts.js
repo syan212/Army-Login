9e67523d852ca24914b7e23b2bc58632174ba437
Siqi Yan <syan235711@gmail.com>
2025-08-25 16:48:27 +0100

    Fix stupid mistake

--- scripts.js
+++ scripts.js
@@ -28,7 +28,7 @@ function copy_token(){
 // Login function
 var token;
 async function login(username, passw){
-    const response = await fetch('https://army-api.onrender.com/', {
+    const response = await fetch('https://army-api.onrender.com/login', {
       	method: 'POST',
       	headers: {
         	'Content-Type': 'application/json',
